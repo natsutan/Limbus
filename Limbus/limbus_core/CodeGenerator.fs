f758d52f94a7242e1e8d58dbc03499bd60005eae
natsutan <YRW03704@nifty.com>
2018-05-18 08:49:35 +0900

    やっぱりPythonにしたい

--- Limbus/limbus_core/CodeGenerator.fs
+++ Limbus/limbus_core/CodeGenerator.fs
@@ -6,13 +6,26 @@ open common
 type CodeGenerator(iCode : iCode, symTab : SymTab) = 
     let mutable iCode = iCode
     let mutable symTab = symTab
+    member this.message_handler = MessegeHandller()    
     member val instruction_count = 0 with set, get
+    
+    interface  MessageProducer with 
+         member this.add_message_listener(ml: MessageListener) : unit = 
+             this.message_handler.add_message_listener(ml)
+         member this.remove_message_listener(ml : MessageListener) : unit = 
+             this.message_handler.remove_message_listener(ml)
+         member this.send_message(m : Message) : unit = 
+             this.message_handler.send_message(m)
+         
+    member this.send_message(m : Message) : unit = 
+        this.message_handler.send_message(m)    
     
     member this.lprocess( iCode : iCode, symTab : SymTab) : unit =
         this.instruction_count <- 0
         //sendmessage
-        let m = 
-        1 |> ignore
+        let ms = string(this.instruction_count)
+        let m = {msg_type = COMPILER_SUMMARY ; body = MSG ms;}
+        this.send_message(m)
         
      
     
--- Limbus/limbus_core/backend.fs
+++ Limbus/limbus_core/backend.fs
@@ -19,3 +19,37 @@ type Backend(iCode : iCode, symTab : SymTab) =
             
     abstract member process : iCode -> SymTab -> unit
     
+
+[<Class>]
+type Interpreter(iCode : iCode, symTab : SymTab) = 
+    let mutable iCode = iCode
+    let mutable symTab = symTab
+    
+    member this.message_handler = MessegeHandller()    
+    member val execution_count = 0 with set, get
+    member val runtime_errres = 0 with set, get
+    
+    
+    interface  MessageProducer with 
+         member this.add_message_listener(ml: MessageListener) : unit = 
+             this.message_handler.add_message_listener(ml)
+         member this.remove_message_listener(ml : MessageListener) : unit = 
+             this.message_handler.remove_message_listener(ml)
+         member this.send_message(m : Message) : unit = 
+             this.message_handler.send_message(m)
+         
+    member this.send_message(m : Message) : unit = 
+        this.message_handler.send_message(m)
+         
+         
+    member this.lprocess(iCode : iCode, symTab : SymTab) : unit =
+        this.execution_count <- 0
+        this.runtime_errres <- 0
+        //sendmessage
+        let es = string(this.execution_count)
+        let rs = string(this.runtime_errres)
+        
+        let m = {msg_type = INTERPRETER_SUMMARY ; body = MSGS [es ; rs];}
+        this.send_message(m)
+               
+                     
