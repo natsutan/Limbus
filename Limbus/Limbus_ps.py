ad2a35056ca0731e64a7325c3673085261cfcc57
夏谷 実/パソナテック <mnatsutani@pasona.co.jp>
2018-06-08 15:06:40 +0900

    chap7終了

--- Limbus/Limbus_ps.py
+++ Limbus/Limbus_ps.py
@@ -5,7 +5,9 @@ from pascal.pascal import Pascal
 #hello_ps = 'pascal_src/case.txt'
 #hello_ps = 'pascal_src/if.txt'
 #hello_ps = 'pascal_src/iferrors.txt'
-hello_ps = 'pascal_src/loops.txt'
+#hello_ps = 'pascal_src/loops.txt'
+#hello_ps = 'pascal_src/repeat.txt'
+hello_ps = 'pascal_src/while.txt'
 
 
 
