e9c0bd5a4820cac8df564cf65db6c03843cc9e12
夏谷 実/パソナテック <mnatsutani@pasona.co.jp>
2018-06-08 15:11:33 +0900

    chap8

--- Limbus/Limbus_ps.py
+++ Limbus/Limbus_ps.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 from pascal.pascal import Pascal
 
-#hello_ps = 'pascal_src/assignments.txt'
-#hello_ps = 'pascal_src/case.txt'
-#hello_ps = 'pascal_src/if.txt'
-#hello_ps = 'pascal_src/iferrors.txt'
-#hello_ps = 'pascal_src/loops.txt'
-#hello_ps = 'pascal_src/repeat.txt'
-hello_ps = 'pascal_src/while.txt'
+hello_ps = 'pascal_src/assignments.txt'
 
 
 
