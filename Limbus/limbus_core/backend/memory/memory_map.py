8422588e480f17d386e13f99cac03ffa344ba7a8
natsutan <YRW03704@nifty.com>
2018-08-07 10:31:24 +0900

    CELL追加

--- Limbus/limbus_core/backend/memory/memory_map.py
+++ Limbus/limbus_core/backend/memory/memory_map.py
@@ -2,6 +2,20 @@ from ... intermidiate.type_impl import Definition, TypeSpec, TypeForm
 from ... intermidiate.symtab_if import SymTabIF
 
 from .. memory_if import MemoryMapIF, create_cell, create_memory_map
+from .. runtime_if import CellIF
+
+
+class Cell(CellIF):
+    def __init__(self, value):
+        self.value = None
+
+    @property
+    def value(self):
+        return self.value
+
+    @value.setter
+    def value(self, v):
+        self.value = v
 
 
 class MemoryMap(MemoryMapIF):
