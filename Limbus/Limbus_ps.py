79e1249c2ada82ac536f563b41c21f79792dd586
natsutan <YRW03704@nifty.com>
2018-08-11 22:52:41 +0900

    ファイル整理

--- Limbus/Limbus_ps.py
+++ Limbus/Limbus_ps.py
@@ -3,7 +3,8 @@ from pascal.pascal import Pascal
 
 #hello_ps = 'pascal_src/assignments.txt'
 #hello_ps = 'pascal_src/declarations.txt'
-hello_ps = 'pascal_src/routines.pas'
+#hello_ps = 'pascal_src/routines.pas'
+hello_ps = 'pascal_src/hello.pas'
 
 def main():
     pascal = Pascal('execute', hello_ps, 'xlicraf')
